64d2c577a190ad15ad43e89e87717d8c925e5ef8
Diego González Liarte <diegogliarte@gmail.com>
2026-06-04 13:51:53 +0200

    add cache on anime details

--- src/routes/api/mal/anime/[id]/+server.ts
+++ src/routes/api/mal/anime/[id]/+server.ts
@@ -3,6 +3,9 @@ import type { AnimeDetailsResponse } from '$lib/types/anime';
 import { fetchAnimeDetails } from '$lib/server/mal';
 import type { RequestHandler } from './$types';
 
+const ONE_DAY = 60 * 60 * 24;
+const ONE_WEEK = ONE_DAY * 7;
+
 export const GET: RequestHandler = async ({ params, fetch }) => {
 	const animeId = Number(params.id);
 
@@ -12,7 +15,21 @@ export const GET: RequestHandler = async ({ params, fetch }) => {
 
 	const data = await fetchAnimeDetails(fetch, animeId);
 
-	return json({
-		data
-	} satisfies AnimeDetailsResponse);
+	return json(
+		{
+			data
+		} satisfies AnimeDetailsResponse,
+		{
+			headers: {
+				'Cache-Control': 'public, max-age=0, must-revalidate',
+				'Netlify-CDN-Cache-Control': [
+					'public',
+					'durable',
+					`s-maxage=${ONE_DAY * 7}`,
+					`stale-while-revalidate=${ONE_WEEK * 4}`
+				].join(', '),
+				'Netlify-Cache-Tag': `anime-details, anime-${animeId}`
+			}
+		}
+	);
 };
